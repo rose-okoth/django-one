2c06dda48fc8c32b7ed339126fb76bb98098660b
rose-okoth <okoth.rose0@gmail.com>
2021-03-20 13:08:10 +0300

    added the models to the admin page

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Editor,Post,tags
 
 # Register your models here.
+admin.site.register(Editor)
+admin.site.register(Post)
+admin.site.register(tags)
+
