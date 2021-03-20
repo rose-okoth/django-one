ca2be045caede18ad6b47c4fc8e00f3b3559d98e
rose-okoth <okoth.rose0@gmail.com>
2021-03-20 13:14:19 +0300

    updated the view function to make quotes_today the landing page

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.quote_of_day,name='quotesToday'),
+    url('^$',views.quote_of_day,name='quotesToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_quotes,name='pastQuotes'),
 ]
