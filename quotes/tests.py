fb01a2d8ce354113ee946fb3a71076ee7a2e60ef
rose-okoth <okoth.rose0@gmail.com>
2021-03-20 12:57:45 +0300

    added editor test case

--- quotes/tests.py
+++ quotes/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Editor,Post,tags
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+# Set up method
+    def setUp(self):
+        self.james= Editor(first_name = 'Rose', last_name ='Okoth', email ='okoth.rose@gmail.com')
+
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Editor))
