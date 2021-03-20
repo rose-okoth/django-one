0e40687f681d0f85022839f47d7dde7a0dff783c
rose-okoth <okoth.rose0@gmail.com>
2021-03-20 13:19:33 +0300

    added a tag filter for the admin page

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Editor,Post,tags
 
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags')
+
 # Register your models here.
 admin.site.register(Editor)
 admin.site.register(Post)
