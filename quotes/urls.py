72b82f63fab49b458eb2fb0e2143f6c8307bc51a
rose-okoth <okoth.rose0@gmail.com>
2021-03-20 12:16:45 +0300

    added in the navbar

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.quote_of_day,name='quoteToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_quotes,name = 'pastQuotes'),
+    url('^today/$',views.quote_of_day,name='quotesToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_quotes,name='pastQuotes'),
 ]
