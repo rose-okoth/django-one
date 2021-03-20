3c662b0be62b833ee62b66a65fcd8824841db323
rose-okoth <okoth.rose0@gmail.com>
2021-03-20 11:18:12 +0300

    added fucntionality to display the day of the week

--- quotes/views.py
+++ quotes/views.py
@@ -7,11 +7,23 @@ def welcome(request):
 
 def quote_of_day(request):
     date = dt.date.today()
+
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1> Quote for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
     return HttpResponse(html) 
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
