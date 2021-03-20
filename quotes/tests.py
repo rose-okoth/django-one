130a7a9a99c49536639e2d9299f48d0fc5780d1e
rose-okoth <okoth.rose0@gmail.com>
2021-03-20 13:23:24 +0300

    added post test case

--- quotes/tests.py
+++ quotes/tests.py
@@ -17,3 +17,24 @@ class EditorTestClass(TestCase):
         self.rose.save_editor()
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
+
+class PostTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.rose= Editor(first_name = 'Rose', last_name ='Okoth', email ='okoth.rose@gmail.com')
+        self.rose.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.rose)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Post.objects.all().delete()
