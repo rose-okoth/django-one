28976e2803ef4f09be35c5b35c36839504925604
rose-okoth <okoth.rose0@gmail.com>
2021-03-25 11:26:00 +0300

    added the user registration form

--- nation/urls.py
+++ nation/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.urls import include, path
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('quotes.urls'))
+    url(r'',include('quotes.urls')),
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/profile/'),
+        name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
